93fbb36b29aad1bbb1d5f4adf7d911ed82a4a43e
226416 Mirnejhad <226416@kedst.ac.uk>
2023-09-19 12:39:45 +0100

    node pairing

--- merkletree2.py
+++ merkletree2.py
@@ -29,6 +29,7 @@ class Node:
     def __init__(self, dataset):
         self.dataset = dataset
         self.node = []
+        self.fill()
 
     def fill(self):
         self.item = item_transfer(self.dataset)
@@ -36,10 +37,29 @@ class Node:
         self.hash = hashlib.sha256(self.hash_input).hexdigest()
         self.node.append(self.hash)
 
-    def reveal(self):
-        print(self.node)
+    def repr(self):
+        return str(self.node[0])
 
 # ^^^ allocate half the dataset to nodes in pairs and represent data in nodes as hashes ^^^
+
+class merkleTree:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.tree = []
+
+    def generate(self):
+        while len(self.dataset) > 0:
+            if len(self.dataset) > 1:
+                node_pair = [Node(self.dataset).repr(), Node(self.dataset).repr()]
+                self.tree.append(node_pair)
+            elif len(self.dataset) == 1:
+                node1 = Node(self.dataset).repr()
+                node2 = f"{node1}"
+                node_pair = [node1, node2]
+                self.tree.append(node_pair)
+        for pair in self.tree:
+            hash_input = pair[0] + pair[1]
+            
 
 # ^^^ allocate half of the new smaller dataset to the node pairs, represented by a hash using the contatenated hashes of the child nodes ^^^
 
